38f1499cf23c0b47bde27dc33f57a3d1cc573c7a
Santos, Lindalva <Lindalva.Santos@mastercard.com>
2023-03-07 13:48:06 -0600

    addding first test suite

---
+++ src/test/java/suites/TestSuite01.java
@@ -0,0 +1,24 @@
+package suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import scripts.HomePageLoadTest;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({ 
+	HomePageLoadTest.class	
+})
+
+public class TestSuite01 {
+
+}
+
+
+
+
+
+
+
+
